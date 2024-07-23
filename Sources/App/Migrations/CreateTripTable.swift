5c2689b45d7b7092061868704b98d1a18f7a4b2f
sirichartchai <gs3802@nyu.edu>
2024-07-23 11:15:15 -0400

    implemented trip migration

---
+++ Sources/App/Migrations/CreateTripTable.swift
@@ -0,0 +1,26 @@
+import Foundation
+import Fluent
+
+
+struct TripMigration:AsyncMigration{
+    func prepare(on database:Database) async throws{
+        try await database.schema("trip")
+            .field("id",.int,.identifier(auto:true))
+            .field("trip_name",.string)
+            .field("ship_date",.date)
+            .field("ship_via",.string)
+            .field("driver_id",.uuid,.required,.references("driver", "id"))
+            .field("total_weight",.double)
+            .field("remark",.string)
+            .field("progress",.string)
+            .field("pick",.int)
+            .field("qty",.int)
+            .create()
+
+    }
+    
+    func revert(on database:Database) async throws{
+        try await database.schema("trip").delete()
+    }
+
+}
